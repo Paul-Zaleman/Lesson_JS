8a31bff730cac519d89cb5d5dd179601a064c998
Max Kazakov <zalemanpaul@gmail.com>
2022-08-12 11:32:35 +0500

    feat: Completed the task of the lesson

--- script.js
+++ script.js
@@ -11,9 +11,29 @@
 
 let money = 200000;
 let income = 'developer front-end';
-let addExpenses = 'internet, taxi, shisha, beer';
+let addExpenses = 'Internet, Taxi, shisha, beer';
 let deposit = true;
 let mission = 250000;
 let period = 12;
 
 
+/*
+2) Используя методы и свойства:
+   - Вывести в консоль тип данных значений переменных money, income, deposit;
+   - Вывести в консоль длину строки addExpenses
+   - Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
+   - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+   - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+   - Вывести в консоль budgetDay
+*/
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split(', '))
+let budgetDay = money / 30
+console.log(budgetDay.toFixed(2))
