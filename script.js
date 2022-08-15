ee847a6ddc8f9fea3a042f6a2fd9d84024b499b2
Max Kazakov <zalemanpaul@gmail.com>
2022-08-15 22:37:50 +0500

    feat: completed 2 task

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*
 1) Объявить функцию getExpensesMonth. Функция возвращает сумму всех обязательных расходов за месяц
 */
+let profitMonth = +prompt('Введите ваш месячный доход')
 let expensesMonth = prompt('Перечислите расходы за месяц через запятую, после запятой ставьте пробел')
 let arrExpensesMonth = expensesMonth.split(', ');
 let res = 0;
@@ -10,7 +11,21 @@ const getExpensesMonth = () => {
     res = +prompt(`Введите затраты на ${arrExpensesMonth[i]}`)
     total += res
 }
- return alert(`Общая сумма ваших месячных затрат ${total}`)
+ //return alert(`Общая сумма ваших месячных затрат ${total}`)
+return total;
 }
+let totalExpensesMonth = getExpensesMonth()
+alert(`Общая сумма ваших месячных затрат ${total}`)
 
-getExpensesMonth();
+/*
+2) Объявить функцию getAccumulatedMonth. Функция возвращает Накопления за месяц (Доходы минус расходы)
+*/
+
+function getAccumulatedMonth () {
+  let result = profitMonth - totalExpensesMonth
+
+  return alert(`Ваш остаток ${result}` )
+}
+getAccumulatedMonth()
+
+
