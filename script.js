c98e314c6d0e94855185666c99d84f536f66b6f8
Max Kazakov <zalemanpaul@gmail.com>
2022-08-16 08:22:37 +0500

    feat: completed all task

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 1) Объявить функцию getExpensesMonth. Функция возвращает сумму всех обязательных расходов за месяц
 */
 let profitMonth = +prompt('Введите ваш месячный доход')
+let target = +prompt('Введите сумму, которую хотите накопить')
 let expensesMonth = prompt('Перечислите расходы за месяц через запятую, после запятой ставьте пробел')
 let arrExpensesMonth = expensesMonth.split(', ');
 let res = 0;
@@ -20,12 +21,35 @@ alert(`Общая сумма ваших месячных затрат ${total}`)
 /*
 2) Объявить функцию getAccumulatedMonth. Функция возвращает Накопления за месяц (Доходы минус расходы)
 */
-
+let result
 function getAccumulatedMonth () {
-  let result = profitMonth - totalExpensesMonth
+  return result = profitMonth - totalExpensesMonth
+  }
 
-  return alert(`Ваш остаток ${result}` )
+
+/* 
+3) Объявить переменную accumulatedMonth и присвоить ей результат вызова функции getAccumulatedMonth 
+*/
+let accumulatedMonth = getAccumulatedMonth()
+alert(`Ваш остаток ${result}` )
+
+/*
+4) Объявить функцию getTargetMonth. Подсчитывает за какой период будет достигнута цель, зная результат месячного накопления (accumulatedMonth) и возвращает результат
+*/
+let resultTarget
+function getTargetMonth () {
+  return resultTarget = Math.ceil(target  / result)
 }
-getAccumulatedMonth()
+getTargetMonth ()
+alert(`Вы накопите нужную вам сумму через ${resultTarget} месяцев`)
 
 
+/*
+budgetDay высчитываем исходя из значения месячного накопления (accumulatedMonth)
+*/
+let budgetDay
+function getBudgetDay() {
+  return budgetDay = Math.floor(accumulatedMonth / 30)
+}
+getBudgetDay()
+alert(`Ваш бюджет на день ${budgetDay}`)
